d5d5e0381256d801f30df739c98913fb70c69350
Gururaj Chinthamanikar <gurucg144@gmail.com>
2018-04-01 23:56:35 -0400

    Revert "test delete"
    This reverts commit 9664fbd6c22726a8b260712cc19cf35c9635c1a0.

---
+++ src/com/basic/inner/RegularInnerClass.java
@@ -0,0 +1,29 @@
+package com.basic.inner;
+
+public class RegularInnerClass {
+	
+	public static void main (String args []) {
+		
+		System.out.println ("Calling main method");
+		Outer outer = new Outer ();	
+		outer.innerCanaccessOuter();
+	}
+}
+
+class Outer {
+	private int outerint = 100;
+	Inner inner;
+	
+	public Outer () {
+		inner = new Inner ();
+	}
+	public void innerCanaccessOuter () {
+		inner.accessOuter ();
+	}
+	
+	class Inner {
+		public void accessOuter () {
+			System.out.println ("My outer int is " + outerint);
+		}
+ 	}
+}
